b3d0f0fc2a3270b8f1617026edcbeb5d37551f0a
Oskar Figiel <qyzdo@outlook.com>
2020-11-26 00:39:23 +0100

    Setup basic login button control with authorization

--- WakaHub/Controller/WelcomeVC.swift
+++ WakaHub/Controller/WelcomeVC.swift
@@ -6,21 +6,54 @@
 //
 
 import UIKit
+import OAuthSwift
 
 final class WelcomeVC: UIViewController {
+
+    var auth: OAuth?
 
     override func loadView() {
         let view = WelcomeView()
          self.view = view
      }
 
+    var welcomeView: WelcomeView {
+        // swiftlint:disable:next force_cast
+        return view as! WelcomeView
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+
+        
+    }
+
+    private func setupUI() {
         setupNavbar()
+        welcomeView.loginButton.addTarget(self, action: #selector(loginButtonClicked), for: .touchUpInside)
     }
 
     private func setupNavbar() {
         title = "Welcome"
     }
 
+    @objc private func loginButtonClicked() {
+        auth = OAuth()
+        auth?.authorize { result in
+            switch result {
+            case .success(let credentials):
+                KeychainWrapper.shared["Token"] = credentials.oauthToken
+                KeychainWrapper.shared["RefreshToken"] = credentials.oauthRefreshToken
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+
+    private func checkIfLoggedin() -> Bool {
+        let logged = KeychainWrapper.shared["Token"] != nil &&
+            KeychainWrapper.shared["RefreshToken"] != nil ? true : false
+        return logged
+    }
 }
