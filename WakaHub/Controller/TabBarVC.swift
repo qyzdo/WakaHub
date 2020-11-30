ef4ba7c3d3acf0e21d5ef36d3ed7d5ba7cf8a972
Oskar Figiel <qyzdo@outlook.com>
2020-11-30 19:08:21 +0100

    Update TabBarVC.swift
    Seperated everything in functions, changed appearance

--- WakaHub/Controller/TabBarVC.swift
+++ WakaHub/Controller/TabBarVC.swift
@@ -11,17 +11,23 @@ final class TabBarVC: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupAppearance()
+        setupViewControllers()
+    }
 
+    private func setupAppearance() {
+        self.tabBar.backgroundImage = UIImage()
+        self.tabBar.clipsToBounds = true
+    }
+
+    private func setupViewControllers() {
         let firstViewController = StatsVC()
-
         firstViewController.tabBarItem = UITabBarItem(title: "Stats", image: UIImage(systemName: "chart.bar.xaxis"), tag: 0)
 
         let secondViewController = UserVC()
-
         secondViewController.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person.fill"), tag: 1)
 
         let tabBarList = [firstViewController, secondViewController]
-
         viewControllers = tabBarList
     }
 }
