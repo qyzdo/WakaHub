b6694c1b5237c9df2c2fc92019219051de406feb
Oskar Figiel <qyzdo@outlook.com>
2020-12-09 22:31:38 +0100

    Load summaries data

--- WakaHub/Controller/StatsVC.swift
+++ WakaHub/Controller/StatsVC.swift
@@ -13,9 +13,26 @@ final class StatsVC: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         setupNavbar()
+        loadData()
     }
 
     private func setupNavbar() {
         navigationItem.title = "Stats"
+    }
+
+    private func loadData() {
+        let service = ServiceProvider<WakaTimeService>()
+
+        service.load(service: .summaries(startDate: "2020-12-08", endDate: "2020-12-09"), decodeType: Summary.self) { result in
+            switch result {
+            case .success(let response):
+                print(response)
+            case .failure(let error):
+                print(error)
+            case .empty:
+                print("No data")
+
+            }
+        }
     }
 }
