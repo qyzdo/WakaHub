2b00697af953a38432109811837c030a3eae984d
Oskar Figiel <qyzdo@outlook.com>
2020-12-31 01:26:51 +0100

    Created file to store keys, removed keys from repo

---
+++ WakaHub/Networking/OAuthKeys.swift
@@ -0,0 +1,13 @@
+//
+//  OAuthKeys.swift
+//  WakaHub
+//
+//  Created by Oskar Figiel on 31/12/2020.
+//
+
+import Foundation
+
+class OAuthKeys {
+    static let consumerKey =  "yourConsumerKey"
+    static let consumerSecret = "yourConsumerSecret"
+}
