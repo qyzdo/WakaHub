864e05ae073c2b29ac4aabdf15cbd42f32ba2e3d
Oskar Figiel <qyzdo@outlook.com>
2021-01-07 16:43:33 +0100

    Added ipad support

--- WakaHub/View/StatsView/StatsView.swift
+++ WakaHub/View/StatsView/StatsView.swift
@@ -26,14 +26,21 @@ final class StatsView: UIView {
         timeSelectButton.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
         timeSelectButton.leftAnchor.constraint(equalTo: scrollView.leftAnchor).isActive = true
 
-        createChartsWithLabels()
+        switch UIDevice.current.userInterfaceIdiom {
+        case .phone:
+            createChartsWithLabelsPhone()
+        case .pad:
+            createChartsWithLabelsPad()
+        default:
+            createChartsWithLabelsPhone()
+        }
 
         addSubview(activityIndicator)
         activityIndicator.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
         activityIndicator.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
     }
 
-    private func createChartsWithLabels() {
+    private func createChartsWithLabelsPhone() {
         scrollView.addSubview(dailyAverageView)
         dailyAverageView.topAnchor.constraint(equalTo: timeSelectButton.bottomAnchor, constant: 15).isActive = true
         dailyAverageView.leftAnchor.constraint(equalTo: safeArea.leftAnchor).isActive = true
@@ -57,6 +64,35 @@ final class StatsView: UIView {
         scrollView.addSubview(operatingSystemsView)
         operatingSystemsView.topAnchor.constraint(equalTo: editorsView.bottomAnchor, constant: 20).isActive = true
         operatingSystemsView.leftAnchor.constraint(equalTo: safeArea.leftAnchor).isActive = true
+        operatingSystemsView.rightAnchor.constraint(equalTo: safeArea.rightAnchor).isActive = true
+        operatingSystemsView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+    }
+
+    private func createChartsWithLabelsPad() {
+        scrollView.addSubview(categoriesView)
+        categoriesView.topAnchor.constraint(equalTo: timeSelectButton.bottomAnchor, constant: 15).isActive = true
+        categoriesView.leftAnchor.constraint(equalTo: safeArea.leftAnchor).isActive = true
+        categoriesView.rightAnchor.constraint(equalTo: safeArea.rightAnchor).isActive = true
+
+        scrollView.addSubview(dailyAverageView)
+        dailyAverageView.topAnchor.constraint(equalTo: categoriesView.bottomAnchor, constant: 15).isActive = true
+        dailyAverageView.leftAnchor.constraint(equalTo: safeArea.leftAnchor).isActive = true
+        dailyAverageView.rightAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
+
+        scrollView.addSubview(languagesView)
+        languagesView.topAnchor.constraint(equalTo: categoriesView.bottomAnchor, constant: 15).isActive = true
+        languagesView.leftAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
+        languagesView.rightAnchor.constraint(equalTo: safeArea.rightAnchor).isActive = true
+
+        scrollView.addSubview(editorsView)
+        editorsView.topAnchor.constraint(equalTo: languagesView.bottomAnchor, constant: 20).isActive = true
+        editorsView.leftAnchor.constraint(equalTo: safeArea.leftAnchor).isActive = true
+        editorsView.rightAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
+        editorsView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+
+        scrollView.addSubview(operatingSystemsView)
+        operatingSystemsView.topAnchor.constraint(equalTo: languagesView.bottomAnchor, constant: 20).isActive = true
+        operatingSystemsView.leftAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
         operatingSystemsView.rightAnchor.constraint(equalTo: safeArea.rightAnchor).isActive = true
         operatingSystemsView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
     }
